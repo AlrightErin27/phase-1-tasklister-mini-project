8b198f950f688c1313792c19a4d63b5591e5d774
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-01 15:13:21 -0800

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form");
+  const newTaskDescription = document.querySelector("#new-task-description");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    //use cb fxn to store val
+    createTodoList(newTaskDescription.value);
+
+    //reset input
+    form.reset();
+  });
+
+  function createTodoList(todo) {
+    let p = document.createElement("p");
+    let button = document.createElement("button");
+
+    p.innerHTML = todo;
+    button.innerHTML = "remove";
+
+    document.querySelector("#list").appendChild(p);
+    p.appendChild(button);
+
+    button.addEventListener("click", function (e) {
+      e.target.parentNode.remove();
+    });
+  }
 });
